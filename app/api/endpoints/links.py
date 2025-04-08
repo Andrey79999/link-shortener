cf33db3da288dd550911cd2d67d7e501574a2efb
Andrey <126543@gmail.com>
2025-04-08 12:21:30 +0700

    feat: added endpoint to update link

--- app/api/endpoints/links.py
+++ app/api/endpoints/links.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, HTTPException, Depends, Request
 from sqlalchemy.orm import Session
 from typing import List
 from services.auth_service import get_current_user
-from schemas.link_schema import LinkCreate, LinkResponse, LinkDelete, LinkCreateCustom
+from schemas.link_schema import LinkCreate, LinkResponse, LinkDelete, LinkCreateCustom, LinkUpdateCustom
 from models.link import Link, LinkStats
 from models.user import User
 from db.session import get_db
@@ -96,3 +96,24 @@ def delete_links(
     query.delete(synchronize_session=False)
     db.commit()
     return True
+
+
+@router.put("/update-link", response_model=LinkResponse)
+def update_link(
+    updated_data: LinkUpdateCustom,
+    db: Session = Depends(get_db),
+    current_user: User = Depends(get_current_user)
+):
+    db_link = db.query(Link).filter(Link.id == updated_data.link_id, Link.user_id == current_user.id).first()
+    if not db_link:
+        raise HTTPException(status_code=404, detail="Link not found")
+
+    if updated_data.custom_code and db.query(Link).filter(Link.short_code == updated_data.custom_code).first():
+        raise HTTPException(status_code=400, detail="Custom code already exists")
+
+    db_link.original_url = str(updated_data.original_url)
+    db_link.short_code = updated_data.custom_code
+
+    db.commit()
+    db.refresh(db_link)
+    return db_link
